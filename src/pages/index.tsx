da5f2699f609ce5b22625e1fdf20d3ab6d17a988
Cedric Hurst <cedric@spantree.net>
2023-11-17 19:54:47 -0600

    chore: fix broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Learn about Hypotheses Briefs
+            to="/docs/introduction">
+            Learn
           </Link>
         </div>
       </div>
